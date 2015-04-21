6f665ed131fa92cdb904d11cf3ca954eefdb7cb1
Jose Luis Fonseca <jose@ditecnologia.com>
2015-04-21 10:10:50 -0500

    working the menu

---
+++ src/Views/partials/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="nav-side-menu">
+    <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+    <div class="menu-list">
+    <ul id="menu-content" class="menu-content collapse out">
+      <li>
+        <a href="#">
+          <i class="fa fa-dashboard fa-lg"></i> Dashboard
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
