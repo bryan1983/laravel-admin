2d982993d943675bde08b072abc3100cf31424cb
Jose Luis Fonseca <jose@ditecnologia.com>
2015-04-22 11:34:32 -0500

    missing test suite

--- src/Views/partials/nav.blade.php
+++ src/Views/partials/nav.blade.php
@@ -13,7 +13,14 @@
         
       </ul>
       <ul class="nav navbar-nav navbar-right">
-          
+          @if (!Auth::guest())
+          <li class="dropdown">
+              <a href="#" class="navbar-profile dropdown-toggle text-bold" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }}<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                  <li><a href="/auth/logout">Logout</a></li>
+              </ul>
+          </li>
+          @endif
       </ul>
     </div>
   </div>
