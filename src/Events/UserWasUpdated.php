a0c909a6bb915ac124d5360eaf0a50d510bfb3f6
Jose Luis Fonseca <jose@ditecnologia.com>
2015-09-03 16:54:13 -0500

    PSR 2

---
+++ src/Events/UserWasUpdated.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Joselfonseca\LaravelAdmin\Events;
+
+use Illuminate\Queue\SerializesModels;
+
+
+/**
+ * Class UserWasUpdated
+ * @package Joselfonseca\LaravelAdmin\Events
+ */
+class UserWasUpdated extends Event
+{
+
+    use SerializesModels;
+
+    /**
+     * @var
+     */
+    public $user;
+    /**
+     * @var
+     */
+    public $input;
+
+    /**
+     * @param $user
+     * @param $data
+     */
+    public function __construct($user, $data)
+    {
+        $this->user = $user;
+        $this->input = $data;
+    }
+
+}
